ab539b974b3eb972a852b3546cf510da9beda8e4
Danila Zakharov <49668471+1StPersona@users.noreply.github.com>
2023-11-29 10:57:03 +0600

    added try,catch added void SendMail

--- SmtpProg/Program.cs
+++ SmtpProg/Program.cs
@@ -13,27 +13,45 @@ namespace SmtpProg
     {
         static void Main(string[] args)
         {
-            //EMAIL from
-            var Mailfrom = new MailAddress("zdanila027@gmail.com", "ME");
-           //EMAIL TO
-            var Mailto = new MailAddress("zdanila027@gmail.com");
-            //mail 
-            var m = new MailMessage(Mailfrom, Mailto);
-           //Header
-            m.Subject = "TEST";
-            // MailText
-            m.Body = "HELLO WORLD";
+            try
+            {
+                SendMail();
+            }
+            catch {
+                Console.WriteLine("$ Error?");
+            }
+        }
 
-            //медот для отправки файла 
-            m.Attachments.Add(new Attachment("C:/Users/danil/OneDrive/Документы/ComputerScience/SmtpProg/TEST.txt"));
+        public static void SendMail()
+        {
+            try
+            {
+                //EMAIL from
+                var Mailfrom = new MailAddress("zdanila027@gmail.com", "ME");
+                //EMAIL TO
+                var Mailto = new MailAddress("kskfbdoc@gmail.com");
+                //mail 
+                var m = new MailMessage(Mailfrom, Mailto);
+                //Header
+                m.Subject = "TEST";
+                // MailText
+                m.Body = "HELLO WORLD";
 
-            // адрес smtp-сервера и порт, с которого будем отправлять письмо
-            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
-            // логин и пароль
-            smtp.Credentials = new NetworkCredential("zdanila027@gmail.com", "nosrqyfdodtqyttc");
-            smtp.EnableSsl = true;
-            smtp.Send(m);
-            Console.Read();
+                //медот для отправки файла 
+                m.Attachments.Add(new Attachment("C:/Users/danil/OneDrive/Документы/ComputerScience/SmtpProg/TEST.txt"));
+
+                // адрес smtp-сервера и порт, с которого будем отправлять письмо
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+                // логин и пароль
+                smtp.Credentials = new NetworkCredential("zdanila027@gmail.com", "nosrqyfdodtqyttc");
+                smtp.EnableSsl = true;
+                smtp.Send(m);
+                Console.Write("Message Sending");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Eror on Message Sending  {ex.Message}");
+            }
         }
     }
 }
